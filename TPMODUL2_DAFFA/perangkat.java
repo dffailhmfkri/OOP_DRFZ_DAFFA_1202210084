f5826eaaf8549c8037abb007238c2659c411a890
dffailhmfkri <dilhamfik1@gmail.com>
2022-12-04 20:16:09 +0700

    Create perangkat.java

---
+++ TPMODUL2_DAFFA/perangkat.java
@@ -0,0 +1,18 @@
+package TPMODUL2_DAFFA;
+
+public class perangkat {
+    protected String drive;
+    protected int ram;
+    protected float processor;
+
+    public perangkat (String drive, int ram, float processor) {
+        this.drive = drive;
+        this.ram = ram;
+        this.processor = processor;
+
+    }
+
+    public void informasi(){
+        System.out.printf("Perangkat tidak diketahui memiliki drive tipe %s dengan ram sebesar %d gb dan processor secepat %.2f %n", drive, ram, processor);
+    }
+}
