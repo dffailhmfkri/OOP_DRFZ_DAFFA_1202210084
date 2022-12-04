4bfc71df015fc9cc601e84ff8e6787088ac65f08
dffailhmfkri <dilhamfik1@gmail.com>
2022-12-04 20:08:43 +0700

    Update handphone.java

--- MODUL2_DAFFA/handphone.java
+++ MODUL2_DAFFA/handphone.java
@@ -9,15 +9,25 @@ public class handphone extends perangkat {
 
     }
     
-    @override
+    @Override
     public void informasi(){
         if (this.fingerprint == true){
-            System.out.printf(format : "");
+            System.out.printf("Handphone ini memiliki dirve tipe %s dengan ram sebesar %d gb dan processor secepat %.2f Ghz. Selain itu handphone ini juga memiliki fingerprint %n",drive, ram, processor);
         } else {
-            system.printf(format : "");
+            System.out.printf("Handphone ini memiliki dirve tipe %s dengan ram sebesar %d gb dan processor secepat %.2f Ghz. Selain itu handphone ini tidak memiliki fingerprint %n",drive, ram, processor);
         }
         
         }
 
-    
+    public void telephone(int phoneNumber){
+        System.out.printf("Handphone berhasil menyambungkan telfon ke No %s %n",phoneNumber);
+    }
+
+    public void sms(int phoneNumber){
+        System.out.printf("Handphone berhasil mengirim SMS ke No %s %n",phoneNumber);
+    }
+
+    public void sms(int phoneNumber1, int phoneNumber2){
+        System.out.printf("Handphone berhasil mengirim SMS ke No %s dan ke %s %n",phoneNumber1,phoneNumber2);
+    }
 }
