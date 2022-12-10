9f565a1a6422ff500979658bab80b2421924fcb6
dffailhmfkri <dilhamfik1@gmail.com>
2022-12-10 07:51:10 +0700

    Update TransportasiAir.java

--- MODUL2_DAFFA/TransportasiAir.java
+++ MODUL2_DAFFA/TransportasiAir.java
@@ -1,5 +1,24 @@
-package MODUL2_DAFFA
+package MODUL2_DAFFA;
 
 public class TransportasiAir {
+    int jumlahKursi;
+    int biaya;
+
+    TransportasiAir(int jumlahKursi, int biaya){
+        this.jumlahKursi = jumlahKursi;
+        this.biaya = biaya;
+    }
+
+    void informasi(){
+        System.out.println("Transportasi Air jenis yang tidak diketahui dengan kursi berjumlah "+jumlahKursi+"ditetapkan dengan biaya sebesar Rp. "+biaya);
+    }
+
+    void berlayar(){
+        System.out.println("Transportasi Air jenis yang tidak diketahui sedang berlayar");
+    }
+
+    void berlabuh(){
+        System.out.println("Transportasi Air jenis yang tidak diketahui berlabuh di pantai");
+    }
     
 }
