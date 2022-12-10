036b5c5feb5d7ac845a8734f3e2bef1b67cddd71
dffailhmfkri <dilhamfik1@gmail.com>
2022-12-10 07:51:04 +0700

    Update Kapal.java

--- MODUL2_DAFFA/Kapal.java
+++ MODUL2_DAFFA/Kapal.java
@@ -1,5 +1,28 @@
 package MODUL2_DAFFA;
 
-public class Kapal {
+public class Kapal extends TransportasiAir {
     
+    String mesin;
+
+    Kapal(int jumlahKursi,int biaya,String mesin){
+        super(jumlahKursi, biaya);
+        this.mesin = mesin;
+    } 
+    
+    void informasi(){
+        System.out.println("Transportasi Air jenis Sampan dengan kursi berjumlah "+jumlahKursi+"ditetapkan dengan biaya sebesar Rp. "+biaya);
+    }
+
+    void berlayar() {
+        System.out.println("Transportasi Air jenis Kapan sedang berlayar menggunakan mesin "+this.mesin+" dengan kecepatan yang tidak stabil");
+    }
+
+    void berlayar(int kecepatan) {
+        System.out.println("Transportasi Air jenis Kapan sedang berlayar menggunakan mesin "+this.mesin+" dengan kecepatan yang stabil di kisaran "+kecepatan+" knot");
+    }
+
+    void berlabuh(){
+        System.out.println("Transportasi Air jenis Sampan berlabuh di pantai");
+    }
+
 }
